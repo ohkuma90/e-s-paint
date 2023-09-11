70a12f26deab9e13cda068c9f0c1e02de9d7201c
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-11 18:36:03 +0900

    コントローラーの記述

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,17 +4,23 @@ class StocksController < ApplicationController
   end
   
   def new
-    @item = Item.new
+    @stock = Stock.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @stock = Stock.new(stock_params)
+    if @stock.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  
+  private
+
+  def stock_params
+    params.require(:stock).permit(:p_name, :category_id, :color, :gloss, :remaining_in_can, :amount,
+                                  :standard_id, :remarks).merge(user_id: current_user.id)
+  end
+
 end
