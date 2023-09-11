9ed8cb673449c88f19151488f4a4ffdb42c252a9
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-11 10:59:54 +0900

    マイグレーションファイルの修正

--- db/migrate/20230911013840_create_stocks.rb
+++ db/migrate/20230911013840_create_stocks.rb
@@ -2,7 +2,7 @@ class CreateStocks < ActiveRecord::Migration[7.0]
   def change
     create_table :stocks do |t|
       t.string :p_name,            null: false
-      t.string :category,          null: false
+      t.integer :category,         null: false
       t.string :color
       t.string :gloss
       t.integer :remaining_in_can, null: false
