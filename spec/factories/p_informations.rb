30c5ea767815895380f4d216f4da77791bd88498
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-12 20:39:51 +0900

    テストコードの記述

--- spec/factories/p_informations.rb
+++ spec/factories/p_informations.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :p_information do
-    
+    p_name { Faker::Commerce.product_name }
+    category_id { rand(2..Category.count) }
+    amount { 0.15 }
+    standard_id { rand(2..Standard.count) }
+    association :user
   end
 end
