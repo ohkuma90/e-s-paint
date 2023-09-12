fd35e679bd5208c3cd1f6b44e3bd8056e5b51855
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-12 20:07:19 +0900

    フォーム・コントローラーの記述

--- app/controllers/p_informations_controller.rb
+++ app/controllers/p_informations_controller.rb
@@ -1,2 +1,22 @@
 class PInformationsController < ApplicationController
+
+  def new
+    @p_information = PInformation.new
+  end
+
+  def create
+    @p_information = PInformation.new(p_information_params)
+    if @p_information.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def p_information_params
+    params.require(:p_information).permit(:p_name, :category_id,:amount, :standard_id).merge(user_id: current_user.id)
+  end
+
 end
