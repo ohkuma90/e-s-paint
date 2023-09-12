775cc1a6f6d7b85b2d8a95c36e3bdfaacbc5c566
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-12 20:09:39 +0900

    モデルの記述

--- app/models/p_information.rb
+++ app/models/p_information.rb
@@ -1,4 +1,15 @@
 class PInformation < ApplicationRecord
   has_many :stocks
   belongs_to :user
+
+  with_options presence: true do
+    validates :p_name
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :amount, numericality: true
+    validates :standard_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
+  # アクティブハッシュのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :standard
 end
