afbaea4aa8263e269118549e3284e464e48a9d9d
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-12 16:55:18 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "stocks#index"
   devise_for :users
-  resources :users do
-    resources :stocks, only: [:index, :new, :create]
-  end
+  resources :stocks, only: [:index, :new, :create]
 end
