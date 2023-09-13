37364019f790cf97b70c3bf536178bf477c61b7b
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-13 15:19:40 +0900

    編集機能の実装

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,6 @@
 class StocksController < ApplicationController
+  before_action :edit_check, only: :edit
+  before_action :set_stock, only: [:edit, :update]
 
   def index
     stocks = Stock.includes(:user)
@@ -26,11 +28,33 @@ class StocksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @stock.update(stock_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def stock_params
     params.require(:stock).permit(:p_name, :category_id, :color, :gloss, :remaining_in_can, :amount,
                                   :standard_id, :remarks).merge(user_id: current_user.id)
+  end
+
+  def edit_check
+    @stock = Stock.find(params[:id])
+    if @stock.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def set_stock
+    @stock = Stock.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "stocks#index"
   devise_for :users
-  resources :stocks, only: [:index, :new, :create]
+  resources :stocks, only: [:index, :new, :create, :edit, :update]
   resources :p_informations, only: [:new, :create, :show]
 end
